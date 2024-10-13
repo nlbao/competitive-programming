6e3455185d3353edfe36bbe24f251037ab4955d2
Bao Nguyen <nlbao95@gmail.com>
2024-10-12 21:34:32 -0700

    Updates DP to use only O(N) memory.

--- solution/LeetCode/3220-count-the-number-of-winning-sequences.cpp
+++ solution/LeetCode/3220-count-the-number-of-winning-sequences.cpp
@@ -9,7 +9,7 @@ private:
 
     const ll MOD = 1e9 + 7;
 
-    int f[1001][2001][3];
+    int f[2][2001][3];
 
     int charToInt(char ch)
     {
@@ -30,20 +30,26 @@ public:
     {
         int n = s.size();
         int a = charToInt(s[0]);
-        f[1][1000][a] = 1;                // draw
-        f[1][1000 + 1][loseCases[a]] = 1; // Bob wins
-        f[1][1000 - 1][winCases[a]] = 1;  // Bob loses
-        for (int i = 2; i <= n; ++i)
+        f[0][1000][a] = 1;                // draw
+        f[0][1000 + 1][loseCases[a]] = 1; // Bob wins
+        f[0][1000 - 1][winCases[a]] = 1;  // Bob loses
+        int i = 0;
+        for (int pos = 1; pos < n; ++pos)
         {
-            int a = charToInt(s[i - 1]);
+            int a = charToInt(s[pos]);
             int bWin = loseCases[a];
             int bLose = winCases[a];
             int bDraw = a;
+            int preI = i;
+            i ^= 1;
             for (int c = 0; c <= 2000; ++c)
-            { // c = (Bob's points - Alice's points + 1000) at i-1
+                for (int b = 0; b < 3; ++b)
+                    f[i][c][b] = 0;
+            for (int c = 0; c <= 2000; ++c)
+            { // c = (Bob's points - Alice's points + 1000) at preI
                 for (int preB = 0; preB < 3; ++preB)
                 {
-                    ll preF = f[i - 1][c][preB];
+                    ll preF = f[preI][c][preB];
                     if (preF == 0)
                         continue;
                     // bob wins
@@ -69,7 +75,7 @@ public:
         for (int c = 1001; c <= 2000; ++c)
             for (int lastB = 0; lastB < 3; ++lastB)
             {
-                ans = mmod(ans + f[n][c][lastB]);
+                ans = mmod(ans + f[i][c][lastB]);
             }
         return ans;
     }
